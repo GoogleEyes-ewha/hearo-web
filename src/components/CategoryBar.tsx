45374d42f4c91bf970a6338ed18ba632b89243f5
official-Cherry <reebon7@gmail.com>
2024-02-10 16:18:41 +0900

    [REFACTOR] category bar

--- src/components/CategoryBar.tsx
+++ src/components/CategoryBar.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
+import  CategoryButton  from './elements/Category/CategoryButton';
 
-// 개별 카테고리 버튼 컴포넌트 
+const categories = [{id:1, name:'국•반찬•메인요리'}, {id:2, name:'수산•해산•건어물'}, {id:3, name:'유제품'},
+{id:4, name:'간식•과자•떡'}, {id:5, name:'과일•견과•쌀'}, {id:6, name:'정육•가공육•계란'}, {id:7, name:'간편식•밀키트•샐러드'}, {id:8, name:'베이커리'}];
 
-interface CategoryButtonProps {
-    category: string;
-    onClick: () => void;
-}
-
-const CategoryButton: React.FC<CategoryButtonProps> = ({ category, onClick }) => {
+const CategoryBar: React.FC = () => {
     return (
-        <button onClick={onClick}>
-            {category}
-        </button>
-    );
-}
+        <div>
+            {categories.map((category) => (
+                <CategoryButton key={category.id} category={category} />
+            ))}
+        </div>
+    )
+};
 
-export default CategoryButton;
+export default CategoryBar;
+
