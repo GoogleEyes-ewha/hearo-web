188964d060587e0d7b86c688882901410284b338
official-Cherry <reebon7@gmail.com>
2024-02-12 14:37:21 +0900

    임시

--- src/components/elements/Product/ProductCard.tsx
+++ src/components/elements/Product/ProductCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import { useRecoilValue } from 'recoil';
 import { authAtom } from '../../../recoil/atoms/AuthAtom';
 import { favoriteState } from '../../../recoil/atoms/UserAtom';
@@ -21,20 +22,46 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
   }
 
   return (
-    <div className="product-card">
-      <img src={product.img} alt={product.name} style={{ width: '100%' }} />
-      <h3>{product.name}</h3>
-      <p>{product.price.toLocaleString()}원</p>
-      <p>{product.info}</p>
-      {isAuthenticated && (
-        isFavorited ? (
-          <button onClick={handleFavoriteClick}>찜해제하기</button>
-        ) : (
-          <button onClick={handleFavoriteClick}>찜하기</button>
-        )
-      )}
-    </div>
+    <Container className="product-card">
+      <ProductInfo>
+        <h3>{product.name}</h3>
+        <p>{product.price.toLocaleString()}원</p>
+        <p>{product.info}</p>
+        {isAuthenticated && (
+          isFavorited ? (
+            <button onClick={handleFavoriteClick}>찜해제</button>
+          ) : (
+            <button onClick={handleFavoriteClick}>찜하기</button>
+          )
+        )}
+      </ProductInfo>
+    </Container>
   ) 
 };
+
+const Container = styled.div`
+  width: 300px;
+  height: 300px;
+  border-radius: 5px;
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  background: url(product.img), lightgray 50%  / cover no-repeat;
+`
+
+const ProductInfo = styled.div`
+  padding: 16px;
+  background: 
+`
+
+const ProductInfo = styled.div`
+  width: 300px;
+  height: 180px;
+  flex-shrink: 0;
+  border-radius: 5px;
+  background: rgba(255, 255, 255, 0.85);
+`
 
 export default ProductCard;
